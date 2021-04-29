52602804acdaa391b798222dd25f54ca3ab95ac4
Jeffrey Salmond <js947@users.noreply.github.com>
2021-04-29 06:05:02 +0000

    minimum working example

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-import { GraphQLSchema } from 'graphql';
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
  
-export default new GraphQLSchema({
-  query: QueryType,
-});
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+ 
+var app = express();
+app.use('/', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
